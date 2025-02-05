1ba51cb6d352d60ef5c9d5c4f9a7d3dbb1dcc35e
Fredrik Bergelv <fredrik.bergelv@live.se>
2025-02-05 15:37:41 +0100

    hamiltonian

--- Task1.py
+++ Task1.py
@@ -5,5 +5,12 @@ N = 501
 V = -1
 epsilon = 0
 gamma = 0.05
-LDOS_sites = [1, 2, 3 , 5, 10, 50, 100, 251]
+LDOS_sites = [1, 2, 3 , 5, 10, 50, 100, 251] 
 
+def H_matrix(n, e, V):
+    "Create a hamilitonian"
+    return np.diag(V * np.ones(n-1), -1) + np.diag(e * np.ones(n), 0) + np.diag(V * np.ones(n-1), 1)
+    
+H = H_matrix(N, epsilon, V)
+
+print(H)
